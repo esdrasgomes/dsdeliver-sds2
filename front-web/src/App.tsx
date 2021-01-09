345489ce705307b1bf178256b6dceee745f08461
Esdras Gomes <esdrasgoomes@gmail.com>
2021-01-09 16:31:54 -0300

    Finish-front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,16 @@
 import { homedir } from 'os';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import './App.css';
 import Routes from './Routes';
 
 function App() {
 	return (
-		<Routes />
+		<>
+			<Routes />
+			<ToastContainer />
+		</>
 	);
 }
 
--- front-web/src/Orders/types.tsx
+++ front-web/src/Orders/types.tsx
@@ -1,13 +1,21 @@
 export type Product = {
-    id: number; 
+    id: number;
     name: string;
     price: number;
     description: string;
     imageUri: string;
 }
 
-export type OrderLocationdata = {
+export type OrderLocationData = {
     latitude: number;
     longitude: number;
     address: string;
 }
+
+type ProductId = {
+    id: number;
+}
+
+export type OrderPayload = {
+    products: ProductId[];
+} & OrderLocationData;
